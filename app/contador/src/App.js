fe1ddf2546e591ee5dffd2a3af5c67145066e109
victorikellmy <victorikellmyferreira123@gmail.com>
2023-03-14 14:59:57 -0300

    Primeiras aplicaçoes

--- app/contador/src/App.js
+++ app/contador/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [numero, setNumero] = useState(0);
+
+
+  function adicionar(){
+    setNumero(numero+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{numero}</h1><br/>
+      <button onClick={adicionar} >+</button>
     </div>
   );
 }
