139ccdda97fdc74b39b6eadccffdb24bb5a94ea5
victorikellmy <victorikellmyferreira123@gmail.com>
2023-03-14 15:43:31 -0300

    Adiçao da funçao diminuir

--- app/contador/src/App.js
+++ app/contador/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 
 function App() {
   const [numero, setNumero] = useState(0);
-
+  
+  function diminuir(){
+    setNumero(numero-1)
+  }
 
   function adicionar(){
     setNumero(numero+1)
@@ -11,6 +14,7 @@ function App() {
   return (
     <div>
       <h1>{numero}</h1><br/>
+      <button onClick={diminuir}>-</button>
       <button onClick={adicionar} >+</button>
     </div>
   );
