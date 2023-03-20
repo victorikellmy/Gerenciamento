5bd7238954fb14ae582d103e9eeac876c387df05
victorikellmy <victorikellmyferreira123@gmail.com>
2023-03-20 09:59:51 -0300

    voltando ao inicio

--- app/contador/src/App.js
+++ app/contador/src/App.js
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 
 function App() {
   const [numero, setNumero] = useState(0);
-  
-  function diminuir(){
-    setNumero(numero-1)
-  }
 
   function adicionar(){
     setNumero(numero+1)
@@ -14,7 +10,6 @@ function App() {
   return (
     <div>
       <h1>{numero}</h1><br/>
-      <button onClick={diminuir}>Diminuir</button>
       <button onClick={adicionar} >Adiconar</button>
     </div>
   );
