c71ade597936a17aeff760df1d4dd6fdfd1df67a
victorikellmy <victorikellmyferreira123@gmail.com>
2023-03-20 10:11:12 -0300

    Botao de Diminuir no site

--- app/contador/src/App.js
+++ app/contador/src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 
 function App() {
   const [numero, setNumero] = useState(0);
+  
+  function diminuir(){
+    setNumero(numero-1)
+  }
 
   function adicionar(){
     setNumero(numero+1)
@@ -10,6 +14,7 @@ function App() {
   return (
     <div>
       <h1>{numero}</h1><br/>
+      <button onClick={diminuir}>Diminuir</button>
       <button onClick={adicionar} >Adiconar</button>
     </div>
   );
